7ccc8b01f9b4eed9b733f247d2b59ab7797c59f8
Noah <noah.labrecue@gmail.com>
2022-04-22 16:01:39 -0400

    Added support for padges

--- src/index.ts
+++ src/index.ts
@@ -23,22 +23,44 @@ let step = () => {};
 let last = performance.now();
 let frames = 0;
 let frametimes:number[] = [];
+
+let perfCont = document.createElement("div");
+perfCont.classList.add("perfCont")
+let frametime = document.createElement("div");
+let entityCount = document.createElement("div");
+
+frametime.classList.add("perf")
+entityCount.classList.add("perf")
+
+perfCont.appendChild(frametime)
+perfCont.appendChild(entityCount)
+
+document.body.appendChild(perfCont);
+
+
+let ecount = 0;
 step = () => {
 	let now = performance.now()
 	let delta = now - last;
 	frametimes.push(delta);
+	frametime.textContent = `fps:${(1000/delta).toFixed(1)}`;
 	last = now;
 	registry.updateSystems(delta);
 
 	for(let i = 0; i < 10; i++){
 		Sonic(registry, Math.random(), Math.random(), Math.random(), Math.random());
 	}
+	ecount+=10;
 
-	if(frames < 5000){
-		frames = requestAnimationFrame(step);
-	} else {
-		console.log(frametimes)
-	}
+	entityCount.textContent = `${ecount}`;
+	frames = requestAnimationFrame(step);
+
+
+	// if(frames < 5000){
+	// 	frames = requestAnimationFrame(step);
+	// } else {
+	// 	console.log(frametimes)
+	// }
 }
 
 step();
